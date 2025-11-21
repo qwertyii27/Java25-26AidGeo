a15945136587254c384b4f0956b2ff3b2e072bf5
qwertyii27 <27gtournas@derryfield.org>
2025-11-21 12:04:43 -0500

    Finished DimenstionTest
    The rows and colums can now be read and turned into arrays.

--- DimensionTest.java
+++ DimensionTest.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class DimensionTest {
     public static void main(String[] args) {
-        String myStr = "Grid 01\n" + //
-                "003020600\n" + //
-                "900305001\n" + //
-                "001806400\n" + //
-                "008102900\n" + //
-                "700000008\n" + //
-                "006708200\n" + //
-                "002609500\n" + //
-                "800203009\n" + //
-                "005010300";
+        String myStr = "Grid 02\n" + //
+                "200080300\n" + //
+                "060070084\n" + //
+                "030500209\n" + //
+                "000105408\n" + //
+                "000000000\n" + //
+                "402706000\n" + //
+                "301007040\n" + //
+                "720040060\n" + //
+                "004010003";
         Scanner in = new Scanner(myStr);
         in.nextLine();
         String[] numsTxt = in.nextLine().split("");
@@ -105,11 +105,18 @@ public class DimensionTest {
         for (int i = 0; i < 9; i++)
             grid1[8][i] = row9[i];
 
+        // print test below
+        System.out.printf("     Grid #02%n%n");
         for (int i = 0; i < 9; i++) {
             for (int n = 0; n < 9; n++) {
                 System.out.print(grid1[i][n]);
+                System.out.print(" ");
+                if (n == 2 || n == 5)
+                    System.out.print("|");
             }
             System.out.println();
+            if (i == 2 || i == 5)
+                System.out.println("-------------------");
         }
         in.close();
     }
