068bc166aa3c64af8237511f42ff30c321ecd872
AidMag <27amagnus@derryfield.org>
2025-12-03 09:35:55 -0500

    Create Permuted_multiples.java

---
+++ Permuted_multiples.java
@@ -0,0 +1,20 @@
+/*
+It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+Find the smallest positive integer, 𝑥, such that 2⁢𝑥, 3⁢𝑥, 4⁢𝑥, 5⁢𝑥, and 6⁢𝑥, contain the same digits. */
+
+
+public class Permuted_multiples {
+    public static void main(String[] args) {
+
+        int a = 1; //a = x
+
+        while (true){
+            if (x)
+
+
+        }
+
+
+    }
+}
