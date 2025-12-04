12e2018a53433b78fdd6bd534961c31d451f7bc1
qwertyii27 <27gtournas@derryfield.org>
2025-12-04 09:46:01 -0500

    i'm so done....

--- Euler52.java
+++ Euler52.java
@@ -1,19 +1,24 @@
 public class Euler52 {
     public static void main(String[] args) {
+        // overall solve condition
         boolean solved = false;
+        // solve conditions for 2x 3x 4x 5x and 6x
         boolean solvedbig2 = false, solvedbig3 = false, solvedbig4 = false, solvedbig5 = false, solvedbig6 = false;
-        int lol = 142000;
+        // starting number to check for
+        int lol = 125874;
         int solvedint = 0;
 
         while (!solved) {
-            int lol2 = lol * 2, lol3 = lol * 3, lol4 = lol * 4, lol5 = lol * 5, lol6 = lol * 6;
-            int solveThreshold = Integer.toString(lol2).length();
-            if (solveThreshold < 6) {
+            int lolLength = Integer.toString(lol).length();
+            // if the number being checked is less than six digits, continue to save time.
+            if (lolLength < 6) {
                 continue;
             }
-            for (int i = 0; i < Integer.toString(lol2).length(); i++) {
+            // lol2, lol3, lol4, lol5, and lol6 are checks for 2x, 3x, 4x, 5x, and 6x.
+            int lol2 = lol * 2, lol3 = lol * 3, lol4 = lol * 4, lol5 = lol * 5, lol6 = lol * 6;
+            for (int i = 0; i < lolLength; i++) {
                 // lol3
-                for (int g = 0; g < solveThreshold; g++) {
+                for (int g = 0; g < lolLength; g++) {
                     if (Integer.toString(lol2).charAt(i) == Integer.toString(lol3).charAt(g)) {
                         solvedint++;
                     }
@@ -22,9 +27,10 @@ public class Euler52 {
                     solvedbig2 = true;
                     solvedbig3 = true;
                 }
+
                 solvedint = 0;
                 // lol4
-                for (int g = 0; g < solveThreshold; g++) {
+                for (int g = 0; g < lolLength; g++) {
                     if (Integer.toString(lol2).charAt(i) == Integer.toString(lol4).charAt(g)) {
                         solvedint++;
                     }
@@ -32,9 +38,10 @@ public class Euler52 {
                 if (solvedint == 6) {
                     solvedbig4 = true;
                 }
+
                 solvedint = 0;
                 // lol5
-                for (int g = 0; g < solveThreshold; g++) {
+                for (int g = 0; g < lolLength; g++) {
                     if (Integer.toString(lol2).charAt(i) == Integer.toString(lol5).charAt(g)) {
                         solvedint++;
                     }
@@ -42,9 +49,10 @@ public class Euler52 {
                 if (solvedint == 6) {
                     solvedbig5 = true;
                 }
+
                 solvedint = 0;
                 // lol6
-                for (int g = 0; g < solveThreshold; g++) {
+                for (int g = 0; g < lolLength; g++) {
                     if (Integer.toString(lol2).charAt(i) == Integer.toString(lol6).charAt(g)) {
                         solvedint++;
                     }
@@ -52,6 +60,7 @@ public class Euler52 {
                 if (solvedint == 6) {
                     solvedbig6 = true;
                 }
+
                 solvedint = 0;
             }
             if (solvedbig2 && solvedbig3 && solvedbig4 && solvedbig5 && solvedbig6) {
---
+++ Euler52_2.java
@@ -0,0 +1,10 @@
+//my brain is so fried i just decided to make a second attempt....
+//explanation for solving: This time around I will try to sort the numbers before
+//checking them.
+public class Euler52_2 {
+    public static void main(String[] args) {
+
+        int x = 0; // OG CountNumber
+        int twoX, threeX, fourX, fiveX, sixX; // self explanatory
+    }
+}
